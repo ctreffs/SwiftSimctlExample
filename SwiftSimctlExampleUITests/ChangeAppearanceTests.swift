f73e8a1a14f8c4680d933866ef56fa86616d281f
Christian Treffs <ctreffs@gmail.com>
2020-03-25 16:33:24 +0100

    Add change appearance test

--- SwiftSimctlExampleUITests/ChangeAppearanceTests.swift
+++ SwiftSimctlExampleUITests/ChangeAppearanceTests.swift
@@ -13,4 +13,43 @@ import XCTest
 class ChangeAppearanceTests: XCTestCase {
     lazy var simctl = SimctlClient(SimulatorEnvironment(bundleIdentifier: exampleAppBundleId,
                                                         host: .localhost(port: 8080))!)
+    // run on iPhone 11 Pro
+    func testDifferentAppearances() throws {
+        let app = XCUIApplication.exampleApp
+        app.launch()
+        _ = app.waitForExistence(timeout: 2.0)
+
+        let expDark = expectation(description: "set_appearance_dark")
+        simctl.setDeviceAppearance(.dark) {
+            switch $0 {
+            case .success:
+                expDark.fulfill()
+
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+        }
+
+        wait(for: [expDark], timeout: 5.0)
+        let label = app.staticTexts.element(boundBy: 0)
+        _ = label.waitForExistence(timeout: 1.0)
+        assertSnapshot(matching: label.screenshot().image,
+                       as: .image)
+
+        let expLight = expectation(description: "set_appearance_light")
+        simctl.setDeviceAppearance(.light) {
+            switch $0 {
+            case .success:
+                expLight.fulfill()
+
+            case let .failure(error):
+                XCTFail("\(error)")
+            }
+        }
+
+        wait(for: [expLight], timeout: 5.0)
+        _ = label.waitForExistence(timeout: 1.0)
+        assertSnapshot(matching: label.screenshot().image,
+                       as: .image)
+    }
 }
